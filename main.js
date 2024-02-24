4a4c32fc5f9f945533317d9ec91fbcbdab3799df
John-Oresh <J.Oresotu@gmail.com>
2024-02-24 17:27:47 +0100

    Javascript added

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("Hello World");
+
+const getData = async() => {
+const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    // mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    // body: JSON.stringify(data), body data type must match "Content-Type" header
+  })
+  console.log(response)
+  return response.json()
+
+};
+const data = await getData()
+console.log(data);
